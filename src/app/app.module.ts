bd66a95a83550dea967c054ecbb828822bdf25b8
Your Name <florespuchojuan@outlook.com>
2023-12-07 06:47:27 -0500

    primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,78 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
+import { PagesLayoutComponent } from './layout/pages-layout/pages-layout.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
+import { HeaderComponent } from './layout/components/header/header.component';
+import { PageTitleComponent } from './layout/components/page-title/page-title.component';
+import { SidebarComponent } from './layout/components/sidebar/sidebar.component';
+import { SearchBoxComponent } from './layout/components/header/search-box/search-box.component';
+import { UserBoxComponent } from './layout/components/header/user-box/user-box.component';
 
+import {DevToolsExtension, NgRedux, NgReduxModule} from '@angular-redux/store';
+import {LoadingBarRouterModule} from '@ngx-loading-bar/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import {PERFECT_SCROLLBAR_CONFIG} from 'ngx-perfect-scrollbar';
+import {PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
+import { ConfigActions } from './ThemeOptions/store/config.actions';
+import { ArchitectUIState, rootReducer } from './ThemeOptions/store';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseLayoutComponent,
+    PagesLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    PageTitleComponent,
+    SidebarComponent,
+    SearchBoxComponent,
+    UserBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PerfectScrollbarModule,
+    NgReduxModule,
+    LoadingBarRouterModule,
+    NgbModule,
+    // FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // ChartsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide:
+      PERFECT_SCROLLBAR_CONFIG,
+      // DROPZONE_CONFIG,
+      useValue:
+      DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+      // DEFAULT_DROPZONE_CONFIG,
+    },
+    ConfigActions,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private ngRedux: NgRedux<ArchitectUIState>,
+    private devTool: DevToolsExtension) {
+    this.ngRedux.configureStore(
+    rootReducer,
+    {} as ArchitectUIState,
+    [],
+    [devTool.isEnabled() ? devTool.enhancer() : f => f]
+    );
+
+    }
+ }
